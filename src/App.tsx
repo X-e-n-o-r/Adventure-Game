15cf08d52baf744c360e78846bd3d4f77b550cb8
Xenor <shokot_2005@mail.ru>
2023-05-14 19:55:43 +0300

    yey

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import './App.css'
+import TextOutput from "./components/TextOutput"
+
 
 function App() {
 
   return (
     <>
-      <h1>Text adventure</h1>
+      <div>
+        <div className="terminal-output-wrap">
+          <h2>» Terminal Output</h2>
+          <div className="terminal-output">
+            <TextOutput />
+          </div>
+        </div>
+        <div className="user-input-wrap">
+          <h2>» Choose Your Commands Here</h2>
+          <div className="option-input">
+            <button className="btn">option 1</button>
+            <button className="btn">option 2</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
